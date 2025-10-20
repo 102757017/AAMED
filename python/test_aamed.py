b1cebe3a3ad5641542f6d5d27b312dedec072d50
sunny <hewei@imasenwh.com>
2025-10-20 15:05:29 +0800

    编译后的文件

--- python/test_aamed.py
+++ python/test_aamed.py
@@ -1,8 +1,14 @@
+import os
+import sys
+sys.path.append(r"E:\python\AAMED\python")
+os.add_dll_directory(r"E:\python\AAMED\python\dll") # 如果遇到DLL加载问题，可以解除注释这行
+
 from pyAAMED import pyAAMED
 import cv2
 
 
-imgC = cv2.imread('002_0038.jpg')
+imgC = cv2.imread('contour.jpg')
+
 imgG = cv2.cvtColor(imgC, cv2.COLOR_BGR2GRAY)
 
 aamed = pyAAMED(600, 600)
@@ -10,5 +16,11 @@ aamed = pyAAMED(600, 600)
 aamed.setParameters(3.1415926/3, 3.4,0.77)
 res = aamed.run_AAMED(imgG)
 print(res)
+
+# 调用 drawAAMED 后，imgG 图像上已经绘制了检测到的椭圆
 aamed.drawAAMED(imgG)
-cv2.waitKey()
+
+cv2.imshow('Detected Ellipses', imgG)
+
+cv2.waitKey(0) # 0 表示无限等待，直到用户按下一个键
+cv2.destroyAllWindows() # 按键后关闭所有OpenCV窗口
